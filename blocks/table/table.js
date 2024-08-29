8e3e987edd1eb42a0a9201ed78e7125620bbeac0
mayukhs2904 <mayukhs@adobe.com>
2024-08-29 14:29:45 +0530

    table changes

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -1,34 +1,29 @@
+import { moveInstrumentation } from '../../scripts/scripts.js';
 /*
  * Table Block
  * Recreate a table
  * https://www.hlx.live/developer/block-collection/table
  */
-
-function buildCell(rowIndex) {
-    const cell = rowIndex ? document.createElement('td') : document.createElement('th');
-    if (!rowIndex) cell.setAttribute('scope', 'col');
-    return cell;
-  }
   
   export default async function decorate(block) {
     const table = document.createElement('table');
     const thead = document.createElement('thead');
     const tbody = document.createElement('tbody');
-  
     const header = !block.classList.contains('no-header');
-    if (header) table.append(thead);
-    table.append(tbody);
+    [...block.children].forEach((row, i) => {
+      const tr = document.createElement('tr');
+      moveInstrumentation(row, tr);
   
-    [...block.children].forEach((child, i) => {
-      const row = document.createElement('tr');
-      if (header && i === 0) thead.append(row);
-      else tbody.append(row);
-      [...child.children].forEach((col) => {
-        const cell = buildCell(header ? i : i + 1);
-        cell.innerHTML = col.innerHTML;
-        row.append(cell);
+      [...row.children].forEach((cell) => {
+        const td = document.createElement(i === 0 && header ? 'th' : 'td');
+  
+        if (i === 0) td.setAttribute('scope', 'column');
+        td.innerHTML = cell.innerHTML;
+        tr.append(td);
       });
+      if (i === 0 && header) thead.append(tr);
+      else tbody.append(tr);
     });
-    block.innerHTML = '';
-    block.append(table);
+    table.append(thead, tbody);
+    block.replaceChildren(table);
   }
